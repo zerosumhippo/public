083bf3fa47574e9ba371c0c38b1c2e2050e7967f
Brendon Reimer <brendonsreimer@gmail.com>
2023-11-29 07:07:36 -0600

    small update

--- other_apps/upgraded-blog/main.py
+++ other_apps/upgraded-blog/main.py
@@ -6,9 +6,10 @@ from wtforms import StringField, SubmitField, URLField
 from wtforms.validators import DataRequired, URL
 from flask_ckeditor import CKEditor, CKEditorField
 from datetime import date
+import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config['CKEDITOR_PKG_TYPE'] = 'full'
 Bootstrap5(app)
 ckeditor = CKEditor(app)
