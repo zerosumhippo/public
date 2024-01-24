3fd53e6dde1d55ff738563cbb34a24a5741a1bd1
Brendon Reimer <brendonsreimer@gmail.com>
2024-01-24 06:23:41 -0600

    db environ var

--- other_apps/upgraded-blog-with-auth/main.py
+++ other_apps/upgraded-blog-with-auth/main.py
@@ -29,7 +29,7 @@ gravatar = Gravatar(app,
 
 
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI", "sqlite:///blog.db")
 db = SQLAlchemy()
 db.init_app(app)
 
