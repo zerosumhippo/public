cac62a436ee528b3bf291587ef3ac57949efa76a
Brendon Reimer <brendonsreimer@gmail.com>
2024-12-15 11:15:05 -0600

    get_sql_file_paths_for_client

--- automation_station/dataset_mgmt/dataset_management.py
+++ automation_station/dataset_mgmt/dataset_management.py
@@ -5,6 +5,12 @@
 # Then it would reach into github to grab the SQL
 # Then update in Redshift
 # Then run metadata generation
+from github_api import GitHubPuller
+from mock_hestia import Hestia
+
+hestia = Hestia()
+ghpuller = GitHubPuller()
+
 
 class DatasetManagement:
     """Finds OneViews for an agency or client in GitHub and runs the SQL scripts."""
@@ -12,5 +18,13 @@ class DatasetManagement:
     def __init__(self):
         pass
 
-    def update_client_oneviews(self, client_id, org_shell_oneview_list, redshift_cluster_id):
-        pass
+    def get_sql_file_paths_for_client(self, client_id):
+        client_schema_name = hestia.get_client_metadata(client_id=client_id)["schema name"]
+        organization_schema_name = hestia.get_org_metadata(
+            org_id=hestia.get_client_metadata(client_id=client_id)["organization id"]
+        )["schema name"]
+        org_shell_sql_files = ghpuller.get_sql_files_in_org_shell_script(organization_schema_name)
+        client_folder_sql_files = ghpuller.get_sql_files_specific_to_client(organization_schema_name,
+                                                                            client_schema_name)
+        return org_shell_sql_files + client_folder_sql_files
+        # need to add logic to remove oneviews from org_shell_sql_files if they exist in client_folder_sql_files
--- automation_station/dataset_mgmt/main.py
+++ automation_station/dataset_mgmt/main.py
@@ -1,13 +1,16 @@
 from github_api import GitHubPuller
 from mock_hestia import Hestia
+from dataset_management import DatasetManagement
 import re
 
 hestia = Hestia()
 ghpuller = GitHubPuller()
+dm = DatasetManagement()
 
-print(ghpuller.get_sql_files_in_org_shell_script("mars_fleet"))
-print(ghpuller.get_sql_files_specific_to_client("starship_agency", "crush_bugs_corp"))
-print(len(hestia.all_org_metadata["organizations"]))
+# print(ghpuller.get_sql_files_in_org_shell_script("mars_fleet"))
+# print(ghpuller.get_sql_files_specific_to_client("starship_agency", "crush_bugs_corp"))
+# print(len(hestia.all_org_metadata["organizations"]))
+print(dm.get_sql_file_paths_for_client(client_id=24689))
 # test_str = "shell_scripts/mars_fleet/v_oneview_suppressive_fire.sql"
 # temp = re.compile("v_oneview_.*.sql")
 # res = temp.search(test_str)
