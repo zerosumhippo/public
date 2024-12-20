1777c29c1d619738c2f05e98e4e5895bbe6f0922
Brendon Reimer <brendonsreimer@gmail.com>
2024-12-20 07:23:35 -0600

    dm get_sql_file_paths_for_client

--- automation_station/dataset_mgmt/dataset_management.py
+++ automation_station/dataset_mgmt/dataset_management.py
@@ -18,12 +18,20 @@ class DatasetManagement:
     def __init__(self):
         pass
 
-    # def _prioritize_client_folder_over_org_shell(self, org_shell_contents, client_folder_contents):
-    #     org_shell_file_name_list = org_shell_contents["file names"]
-    #     client_folder_file_name_list = client_folder_contents["file names"]
-    #     org_shell_file_path_list = org_shell_contents["file paths"]
-    #     client_folder_file_path_list = client_folder_contents["file paths"]
-    #     final_file_path_list = [file_name for file_name in org_shell_contents["file paths"] if ]
+    # def _only_retain_oneview_files(self, github_repo_contents_dict):
+    #     for n in range(len(github_repo_contents_dict["file names"]) - 1):
+    #         if "oneview" not in github_repo_contents_dict["file names"][n]:
+    #             github_repo_contents_dict["file names"].remove(github_repo_contents_dict["file names"][n])
+    #             github_repo_contents_dict["file paths"].remove(github_repo_contents_dict["file paths"][n])
+
+    def _prioritize_client_folder_over_org_shell(self, org_shell_contents, client_folder_contents):
+        org_shell_file_path_indeces = [org_shell_contents["file names"].index(file_name) for file_name
+                                       in org_shell_contents["file names"] if file_name not in
+                                       client_folder_contents["file names"]]
+        sql_files_to_execute_list = [file_path for file_path in org_shell_contents["file paths"]
+                                     if org_shell_contents["file paths"].index(file_path)
+                                     in org_shell_file_path_indeces]
+        return sql_files_to_execute_list + client_folder_contents["file paths"]
 
     def get_sql_file_paths_for_client(self, client_id):
         client_schema_name = hestia.get_client_metadata(client_id=client_id)["schema name"]
@@ -32,6 +40,4 @@ class DatasetManagement:
         )["schema name"]
         org_shell_contents = ghpuller.get_sql_files_in_org_shell_script(organization_schema_name)
         client_folder_contents = ghpuller.get_sql_files_specific_to_client(organization_schema_name, client_schema_name)
-        # self._prioritize_client_folder_over_org_shell(org_shell_contents)
-        return org_shell_contents
-        # need to add logic to remove oneviews from org_shell_sql_files if they exist in client_folder_sql_files
+        return self._prioritize_client_folder_over_org_shell(org_shell_contents, client_folder_contents)
